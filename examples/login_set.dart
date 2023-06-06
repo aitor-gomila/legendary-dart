5110168b10da482783e50744f203f5a30bb99b85
aitor-gomila <aitor-gomila@outlook.com>
2023-06-06 12:03:06 +0200

    Implement `login_set.dart` examples

---
+++ examples/login_set.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:legendary/legendary.dart';
+
+void main(argv) async {
+  print("This will set your Legendary login.");
+
+  final line = argv[0];  
+
+  if (line == null) return;
+
+  print ("Write your login code below:");
+
+  final code = stdin.readLineSync();
+
+  if (code == null) return;
+
+  final client = LegendaryClient(legendaryPath: line);
+  await client.setLogin(code);
+  print("Logged in");
+
+}
