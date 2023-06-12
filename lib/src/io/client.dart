dadd79fd51287124fd7cbe07b1a49fe6e2462303
aitor-gomila <aitor-gomila@outlook.com>
2023-06-13 00:44:12 +0200

    Adapt LegendaryProcessClient to use static LegendaryStreamClient

--- lib/src/io/client.dart
+++ lib/src/io/client.dart
@@ -2,102 +2,132 @@ import 'dart:io';
 import 'dart:convert';
 
 import 'package:legendary/legendary.dart';
+import 'package:legendary/src/io/stream_client.dart';
 
 // This client is intended for end users.
 // You spawn the class, give it the path to legendary,
 // and it just works
-class LegendaryProcessClient extends LegendaryBaseClient {
+class LegendaryProcessClient extends LegendaryStreamClient implements LegendaryBaseClient {
   final String legendaryPath;
   final bool verbose;
-  late final LegendaryStreamClient streamClient;
 
   LegendaryProcessClient({
     required this.legendaryPath,
     this.verbose = false,
-  }) {
-    streamClient = LegendaryStreamClient(
-      startProcess: _runLegendaryCommand,
-      verbosePrint: (message) {
-        if (verbose) stderr.write(message);
-      },
-    );
-  }
+  });
 
-  Future<LegendaryProcess> _runLegendaryCommand(List<String> args) async {
-    final process = await Process.start(
+  Future<Process> _runLegendaryCommand(List<String> args) async {
+    return await Process.start(
       legendaryPath,
       [...args, "--json"]
     );
+  }
+  
+  @override
+  Future<void> cleanup() async {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["cleanup"]);
+    final stderr = process.stderr.transform(utf8.decoder);
 
-    return LegendaryProcess(
-      stdout: process.stdout.transform(utf8.decoder),
-      stderr: process.stderr.transform(utf8.decoder)
-    );
+    return LegendaryStreamClient.cleanup(stderr);
   }
   
   @override
-  Future<void> cleanup() {
-    return streamClient.cleanup();
+  Future<void> deleteLogin() async {
+    final process = await _runLegendaryCommand(["auth", "--delete"]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    return LegendaryStreamClient.deleteLogin(stderr);
   }
   
   @override
-  Future<void> deleteLogin() {
-    return streamClient.deleteLogin();
+  Stream<int> import(String appName, String location) async* {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["move", appName, location]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    yield* LegendaryStreamClient.import(stderr, appName, location);
   }
   
   @override
-  Stream<int> import(String appName, String location) {
-    return streamClient.import(appName, location);
+  Future<InstalledGame> info(String appName) async {
+    final process = await _runLegendaryCommand(["info", appName]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    return LegendaryStreamClient.info(stderr, appName);
   }
   
   @override
-  Future<InstalledGame> info(String appName) {
-    return streamClient.info(appName);
+  Stream<int> install(String appName) async* {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["install", appName]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    yield* LegendaryStreamClient.install(stderr, appName);
   }
   
   @override
-  Stream<int> install(String appName) {
-    return streamClient.install(appName);
+  Stream<String> launch(String appName) async* {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["launch", appName]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    yield* LegendaryStreamClient.launch(stderr, appName);  }
+  
+  @override
+  Future<List<Game>> list() async {
+    final process = await _runLegendaryCommand(["list"]);
+    final stdout = process.stdout.transform(utf8.decoder);
+
+    return LegendaryStreamClient.list(stdout);
   }
   
   @override
-  Stream<String> launch(String appName) {
-    return streamClient.launch(appName);
+  Future<List<InstalledGame>> listInstalled() async {
+    final process = await _runLegendaryCommand(["list-installed"]);
+    final stdout = process.stdout.transform(utf8.decoder);
+
+    return LegendaryStreamClient.listInstalled(stdout);  }
+  
+  @override
+  Stream<int> move(String appName, String path) async* {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["move", appName, path]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    yield* LegendaryStreamClient.move(stderr, appName, path);  }
+  
+  @override
+  Future<void> setLogin(String code, {String? sid, String? token}) async {
+    final process = await _runLegendaryCommand(["cleanup"]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    return LegendaryStreamClient.setLogin(stderr, code, sid: sid, token: token);
   }
   
   @override
-  Future<List<Game>> list() {
-    return streamClient.list();
+  Future<Status> status() async {
+    final process = await _runLegendaryCommand(["status"]);
+    final stdout = process.stdout.transform(utf8.decoder);
+
+    return LegendaryStreamClient.status(stdout);  }
+  
+  @override
+  Stream<int> uninstall(String appName) async* {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["uninstall", appName]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    yield* LegendaryStreamClient.uninstall(stderr, appName);
   }
   
   @override
-  Future<List<InstalledGame>> listInstalled() {
-    return streamClient.listInstalled();
-  }
-  
-  @override
-  Stream<int> move(String appName, String path) {
-    return streamClient.move(appName, path);
-  }
-  
-  @override
-  Future<void> setLogin(String code, {String? sid, String? token}) {
-    return streamClient.setLogin(code, sid: sid, token: token);
-  }
-  
-  @override
-  Future<Status> status() {
-    return streamClient.status();
-  }
-  
-  @override
-  Stream<int> uninstall(String appName) {
-    return streamClient.uninstall(appName);
-  }
-  
-  @override
-  Stream<int> verify(String appName) {
-    return streamClient.verify(appName);
+  Stream<int> verify(String appName) async* {
+    // FIXME: not sure whether to use stdout or stderr
+    final process = await _runLegendaryCommand(["cleanup", appName]);
+    final stderr = process.stderr.transform(utf8.decoder);
+
+    yield* LegendaryStreamClient.verify(stderr, appName);
   }
 
 }
